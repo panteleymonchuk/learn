8f4813525f30bc915378a6f7fdf8519d58532809
Eugene <panteleymonchuk@gmail.com>
2017-07-09 00:46:05 +0300

    С горем пополам готово

--- angular-codeguide-giphy/src/app/app.component.ts
+++ angular-codeguide-giphy/src/app/app.component.ts
@@ -1,24 +1,31 @@
-import { Component } from '@angular/core';
-
-var myVar: string;
-
-function myFunc(msg: string): string {
-  return 'test' + msg;
-}
+import {Component} from '@angular/core';
+import {Http, Response} from '@angular/http';
+// import {NgFor} from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  // "styles": [
-  //   "./node_modules/bootstrap/dist/css/bootstrap.min.css",
-  //   "styles.css"
-  // ],
-  styleUrls: ['./app.component.scss']
+    moduleId: module.id,
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    title = 'Welcome to GiphySearch';
+    link = 'https://api.giphy.com/v1/gifs/search?api_key=15110f24178d425e90bb0bd32559b6f5&limit=25&offset=0&rating=G&lang=en&q=';
+    // link = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
+    http: Http;
+    giphes = [];
 
-  performSearch(searchTerm: HTMLInputElement): void {
-    alert('button click' + `${searchTerm.value}`);
-  }
+
+    constructor(http: Http) {
+        this.http = http;
+    }
+
+    performSearch(searchTerm: HTMLInputElement): void {
+        let apiLink = this.link + searchTerm.value;
+
+        this.http.request(apiLink)
+            .subscribe((res: Response) => {
+                console.log(this.giphes = res.json().data);
+            });
+    }
 }
